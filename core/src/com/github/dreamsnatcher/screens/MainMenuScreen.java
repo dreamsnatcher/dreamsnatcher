5a1731521573fe5b347908231428612a8fdf6e28
Bone <l.knoch@gmx.at>
2015-04-21 19:47:08 +0200

    resize test

--- core/src/com/github/dreamsnatcher/screens/MainMenuScreen.java
+++ core/src/com/github/dreamsnatcher/screens/MainMenuScreen.java
@@ -9,10 +9,11 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.Align;
 
 public class MainMenuScreen extends com.github.dreamsnatcher.screens.Screen {
 
@@ -30,6 +31,7 @@ public class MainMenuScreen extends com.github.dreamsnatcher.screens.Screen {
         skin = new Skin(Gdx.files.internal("editor/uiskin.json"));
         stage = new Stage();
         table = new Table();
+        table.setFillParent(true);
         table.row();
 
 
